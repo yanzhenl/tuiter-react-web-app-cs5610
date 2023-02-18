e13205c9b71ff692afe2427fa9d6ebf7eb11901f
yanzhenl <li.yanzhe@northeastern.edu>
2023-02-17 23:17:05 -0800

    a5 JavaScript spring 2023

--- public/tuiter/HomeScreen/PostItem.js
+++ public/tuiter/HomeScreen/PostItem.js
@@ -10,7 +10,7 @@ export default [
          name: 'New York Post', handle: 'nypost', time: '23h',
         content: 'Grimes seen reading Kari Marx following split with world\'s richest man Elon Musk',
         image: 'grimes.jpeg', title: `Inspiration4 Mission to Space`,
-        text: `From training to launch to landing, this all-access docuseries rides along' +
-         'with the Inspiration4 crew on the first all-civilian orbital space`,
+        text: `From training to launch to landing, this all-access docuseries rides along
+         with the Inspiration4 crew on the first all-civilian orbital space`,
          comment: '965', repost: '2.4K', like: '4K',},
 ];
--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -5,22 +5,22 @@ const NavigationSidebar = (highlight = 'Home') => {
        <i class="fab fa-twitter"></i></a>
        <!-- continue rest of list, e.g.,
             Home, Explore, Notifications,  Messages, etc. -->
-     <a class="list-group-item ${highlight === "Home" ? "active": ''}" href="../HomeScreen/index.html">
-        <i class="fa fa-home"></i><span class="pos-relative-left-3 d-none d-xl-block">Home</span></a>
-     <a class="list-group-item ${highlight === "Explore" ? "active": ''}" href="../ExploreScreen/index.html">
-        <i class="fa fa-hashtag"></i><span class="pos-relative-left-3 d-none d-xl-block">Explore</span></a>
-     <a class="list-group-item" href="/">
-        <i class="fa fa-bell"></i><span class="pos-relative-left-3 d-none d-xl-block">Notifications</span></a>
-     <a class="list-group-item" href="/">
-        <i class="fa fa-envelope"></i><span class="pos-relative-left-3 d-none d-xl-block">Messages</span></a>
-     <a class="list-group-item" href="/">
-        <i class="fa fa-bookmark"></i><span class="pos-relative-left-3 d-none d-xl-block">Bookmarks</span></a>
-     <a class="list-group-item" href="/">
-        <i class="fa fa-list"></i><span class="pos-relative-left-3 d-none d-xl-block">Lists</span></a>
-     <a class="list-group-item" href="/">
-        <i class="fa fa-user"></i><span class="pos-relative-left-3 d-none d-xl-block">Profile</span></a>
-     <a class="list-group-item" href="/">
-        <i class="fa fa-ellipsis-h"></i><span class="pos-relative-left-3 d-none d-xl-block">More</span></a>
+     <a class="list-group-item d-flex ${highlight === "Home" ? "active": ''}" href="../HomeScreen/index.html">
+        <i class="fa fa-home mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Home</span></a>
+     <a class="list-group-item d-flex ${highlight === "Explore" ? "active": ''}" href="../ExploreScreen/index.html">
+        <i class="fa fa-hashtag mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Explore</span></a>
+     <a class="list-group-item d-flex" href="/">
+        <i class="fa fa-bell mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Notifications</span></a>
+     <a class="list-group-item d-flex" href="/">
+        <i class="fa fa-envelope mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Messages</span></a>
+     <a class="list-group-item d-flex" href="/">
+        <i class="fa fa-bookmark mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Bookmarks</span></a>
+     <a class="list-group-item d-flex" href="/">
+        <i class="fa fa-list mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Lists</span></a>
+     <a class="list-group-item d-flex" href="/">
+        <i class="fa fa-user mt-1"></i><span class="pos-relative-left-3 d-none d-xl-block">Profile</span></a>
+     <a class="list-group-item d-flex" href="/">
+        <i class="fa fa-ellipsis-h mt-1"></i><span class="d-none d-xl-block pos-relative-left-3">More</span></a>
    </div>
    <div class="d-grid mt-2">
      <a href="tweet.html"
