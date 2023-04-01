2a4f2fdd286a8001c2f4f56bf830dcc36b4a56f9
yanzhenl <li.yanzhe@northeastern.edu>
2023-04-01 15:24:13 -0700

    a9 Mongo sp23

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
+
 const API_BASE = process.env.REACT_APP_API_BASE;
 // const API_BASE = "https://tuiter-node-server-app-mongo.onrender.com/api";
 const TUITS_API = `${API_BASE}/tuits`;
-
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
